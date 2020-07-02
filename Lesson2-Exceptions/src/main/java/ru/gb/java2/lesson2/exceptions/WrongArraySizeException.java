2ecff0d7423cb11aae8e3a7318fbec27abbbc6c3
Victor Z <zvictormail@gmail.com>
2020-07-02 10:15:42 +0300

    Exceptions HW Done

---
+++ Lesson2-Exceptions/src/main/java/ru/gb/java2/lesson2/exceptions/WrongArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.gb.java2.lesson2.exceptions;
+
+public class WrongArraySizeException extends Exception {
+
+    public WrongArraySizeException(String message) {
+        super(message);
+    }
+}
