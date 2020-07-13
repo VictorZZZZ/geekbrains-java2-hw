8ac50b69d351cd8c0663d3edc4acb3d7a37176ea
Victor Z <zvictormail@gmail.com>
2020-07-13 11:54:48 +0300

    Done 2 threads.

--- Lesson5-threads/src/main/java/ru/gb/CalcInThread.java
+++ Lesson5-threads/src/main/java/ru/gb/CalcInThread.java
@@ -4,16 +4,14 @@ import static ru.gb.MainClass.calc;
 
 public class CalcInThread extends Thread {
     private float[] arr;
-    private int startIndex;
-    private int endIndex;
-    public CalcInThread(float[] arr,int startIndex,int endIndex) {
+    private int offset;
+    public CalcInThread(float[] arr,int offset) {
         this.arr = arr;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
+        this.offset = offset;
     }
 
     @Override
     public void run() {
-        calc(arr,startIndex,endIndex);
+        calc(arr,offset);
     }
 }
--- Lesson5-threads/src/main/java/ru/gb/MainClass.java
+++ Lesson5-threads/src/main/java/ru/gb/MainClass.java
@@ -1,31 +1,33 @@
 package ru.gb;
 
+import java.util.Arrays;
+
 public class MainClass {
-    static final int size = 10000000;
+    static final int size = 10_000_000;
     static final int h = size / 2;
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        straightCount();
+        float[] straightArr= straightCount();
         System.out.printf("Time of straight method: %dms %n",System.currentTimeMillis()-start);
         //second method
         start = System.currentTimeMillis();
-        threadsCount();
+        float[] threadsArr = threadsCount();
         System.out.printf("Time of threads method: %dms %n",System.currentTimeMillis()-start);
+        System.out.println("Results equals = "+Arrays.equals(straightArr,threadsArr));
     }
 
 
-    private static void straightCount(){
+    private static float[] straightCount(){
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++) {
             arr[i]=1;
         }
-        calc(arr,0,arr.length-1);
-        System.out.println("Straight");
-        System.out.printf("%f %f %f ... %f%n",arr[0],arr[1],arr[2],arr[arr.length-1]);
+        calc(arr,0);
+        return arr;
     }
 
-    private static void threadsCount(){
+    private static float[] threadsCount(){
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++) {
             arr[i]=1;
@@ -33,10 +35,10 @@ public class MainClass {
         float[] a1 =new float[h];
         float[] a2 =new float[h];
         System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h+1, a2, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
 
-        CalcInThread t1 = new CalcInThread(a1,0,h);
-        CalcInThread t2 = new CalcInThread(a2,h+1,arr.length-1);
+        CalcInThread t1 = new CalcInThread(a1,0);
+        CalcInThread t2 = new CalcInThread(a2,h);
         t1.start();
         t2.start();
 
@@ -44,16 +46,16 @@ public class MainClass {
             t1.join();
             t2.join();
             System.arraycopy(a1, 0, arr, 0, h);
-            System.arraycopy(a2, 0, arr, h+1, h);
-            System.out.printf("%f %f %f ... %f%n",arr[0],arr[1],arr[2],arr[arr.length-1]);
+            System.arraycopy(a2, 0, arr, h, h);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        return arr;
     }
 
-    public static void calc(float[] arr,int startIndex,int endIndex){
-        for (int i = startIndex; i < endIndex; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    public static void calc(float[] arr,int offset){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f+(i+offset) / 5) * Math.cos(0.2f + (i+offset) / 5) * Math.cos(0.4f + (i+offset) / 2));
         }
     }
 }
