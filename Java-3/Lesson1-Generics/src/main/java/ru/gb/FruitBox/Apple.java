0e8f7fc5853e493763bc4a2f8ee03f7a0c9e8ec8
Victor <zvictormail@gmail.com>
2020-07-31 17:47:34 +0300

    Lesson1 - generics

---
+++ Java-3/Lesson1-Generics/src/main/java/ru/gb/FruitBox/Apple.java
@@ -0,0 +1,10 @@
+package ru.gb.FruitBox;
+
+public class Apple extends Fruit {
+    public static final float WEIGHT = 1.0f;
+
+    @Override
+    public float getWeight() {
+        return this.WEIGHT;
+    }
+}
